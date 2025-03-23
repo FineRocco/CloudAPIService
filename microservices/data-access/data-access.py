984e146281ba6c33fee45aa794aa34c452064699
GustavoCostaHenriques <gusta.santos.henriques@gmail.com>
2025-03-23 18:13:37 +0000

    Add last logic of averagesalary use case

--- microservices/data-access/data-access.py
+++ microservices/data-access/data-access.py
@@ -2,6 +2,17 @@
 import psycopg2
 import psycopg2.extras
 
+import random, os
+from concurrent import futures
+
+import grpc
+import data_access_pb2_grpc
+from grpc_interceptor import ExceptionToStatusInterceptor
+from grpc_interceptor.exceptions import NotFound
+from data_access_pb2 import (
+    AverageSalaryResponse
+)
+
 class DataAccess:
     def __init__(self, host="localhost", port=5432, database="mydatabase", user="myuser", password="mypassword"):
         self.conn_params = {
@@ -27,6 +38,18 @@ class DataAccess:
         finally:
             conn.close()
     
+    def fetch_jobs_by_title(self, title):
+        """Fetch all rows from the jobs table with a certain title."""
+        conn = self._get_connection()
+        try:
+            with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+                cur.execute("SELECT * FROM jobs WHERE title = %s;", (title,))
+                rows = cur.fetchall()
+                return rows
+        finally:
+            conn.close()
+        
+    
     def fetch_reviews(self):
         """Fetch all rows from the reviews table."""
         conn = self._get_connection()
@@ -48,14 +71,26 @@ class DataAccess:
                 return rows
         finally:
             conn.close()
-
+            
+class GetAverageSalary(data_access_pb2_grpc.DataAccessServiceServicer):
+    def GetAverageSalary(self, request, context):
+        da = DataAccess()
+        jobs = da.fetch_jobs_by_title(request.title)
+        
+        if jobs:
+            avg_salary = sum(job["med_salary"] for job in jobs if job["med_salary"] is not None) / len(jobs)
+        else:
+            avg_salary = 0
+            
+        return AverageSalaryResponse(salary=avg_salary)
+        
 def serve():
     interceptors = [ExceptionToStatusInterceptor()]
     server = grpc.server(
         futures.ThreadPoolExecutor(max_workers=10), interceptors=interceptors
     )
-    jobpostings_pb2_grpc.add_JobPostingServiceServicer_to_server(
-        AverageSalaryService(), server
+    data_access_pb2_grpc.add_DataAccessServiceServicer_to_server(
+        GetAverageSalary(), server
     )
     """ with open("server.key", "rb") as fp:
         server_key = fp.read()
@@ -75,11 +110,4 @@ def serve():
 
 
 if __name__ == "__main__":
-    serve()
-
-
-
-
-if __name__ == '__main__':
-    da = DataAccess()
     serve()
