bffd043da7b13e6d52f7f3bf93cd90be66970126
Leonardo Monteiro <131617928+lixX-17@users.noreply.github.com>
2025-03-27 21:40:37 +0000

    Update api_interface.py

--- microservices/api_interface/api_interface.py
+++ microservices/api_interface/api_interface.py
@@ -2,15 +2,21 @@ import os
 
 import grpc
 from flask import Flask, render_template, request
-from jobpostings_pb2 import AverageSalaryRequest, JobsWithRatingRequest
+from jobpostings_pb2 import AverageSalaryRequest
 from jobpostings_pb2_grpc import JobPostingServiceStub
+from jobreviews_pb2_grpc import JobReviewServiceStub
+from jobreviews_pb2 import BestCityRequest
 
 app = Flask(__name__)
 
 jobpostings_host = os.getenv("JOBPOSTINSHOST", "job-postings")
-
 jobpostings_channel = grpc.insecure_channel(f"{jobpostings_host}:50051")
 job_postings_client = JobPostingServiceStub(jobpostings_channel)
+
+# Conectar ao serviço de Job Reviews (para pegar as melhores cidades)
+jobreviews_host = os.getenv("JOBREVIEWSHOST", "job-reviews")
+jobreviews_channel = grpc.insecure_channel(f"{jobreviews_host}:50051")
+job_reviews_client = JobReviewServiceStub(jobreviews_channel)
 
 @app.route("/")
 def render_homepage():
@@ -29,18 +35,16 @@ def render_homepage():
         averageSalary_response=averageSalary_response.averageSalary
     )
     
-@app.route("/jobsWithRating")
-def render_jobWithRatingPage():
-    
-    jobs_with_rating_request = JobsWithRatingRequest(
-        title="Marketing Coordinator", city="Chicago"
-    )
-    
-    jobs_with_rating_response = job_postings_client.JobsWithRating(
-        jobs_with_rating_request
-    )
+@app.route("/location")
+def render_location():
+    cities_request = BestCityRequest()
+    cities_response = job_reviews_client.BestCity(cities_request)
     
     return render_template(
-        "jobsWithRating",
-        jobs_with_rating_response=jobs_with_rating_response.jobs
+        "location.html",
+        cities=cities_response.city
     )
+    
+if __name__ == "__main__":
+    app.run(debug=True)
+    
