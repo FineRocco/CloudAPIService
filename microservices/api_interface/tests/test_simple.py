e91c8fed5ba01e306ef60991ede2fe0fda7c2708
FineRocco <l3ti.me20@gmail.com>
2025-05-22 16:42:06 +0000

    Remove redundant test_example2 function and clean up test_example in test_simple.py

--- microservices/api_interface/tests/test_simple.py
+++ microservices/api_interface/tests/test_simple.py
@@ -2,7 +2,3 @@
 def test_example():
     print("Running a simple unit test for api-interface!")
     assert True
-
-def test_example2():
-    print("Running a simple unit test2 for api-interface!")
-    assert True
